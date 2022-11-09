5b9c2a5a5f2a04cc10caca19b00c805bfd4bd54d
Fred X <98150516+ffred337@users.noreply.github.com>
2022-11-09 23:18:11 +0100

    Add files via upload
    java source code (src)

---
+++ src/view/WinLosePanel.java
@@ -0,0 +1,49 @@
+package view;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.Observable;
+import java.util.Observer;
+
+public class WinLosePanel extends JPanel implements Observer {
+    private Font small;
+
+    public WinLosePanel() {
+        super();
+        try {
+            File smal = new File("assets/fonts/Retro Gaming.ttf");
+            this.small = Font.createFont(Font.TRUETYPE_FONT, smal);
+        } catch (FontFormatException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void update(Observable observable, Object o) {
+        repaint();
+    }
+
+    public void setVictory(Graphics g) {
+        this.setBackground(Color.black);
+        g.setFont(small.deriveFont(Font.ITALIC, 80f));
+        g.setColor(Color.white);
+        PanelComponent background = new PanelComponent(0, 0, "assets/logo/bg_sky.jpg");
+        PanelComponent trophy = new PanelComponent(0, 0, "assets/logo/trophy.jpg");
+        g.drawImage(background.getImage().getScaledInstance(1440, 768, Image.SCALE_DEFAULT), 5, 0, this);
+        g.drawImage(trophy.getImage().getScaledInstance(600, 600, Image.SCALE_DEFAULT), 400, 0, this);
+        g.drawString("You have won", 350, 700);
+    }
+    public void setDefeat(Graphics g) {
+        g.setFont(small.deriveFont(Font.ITALIC, 80f));
+        this.setBackground(Color.black);
+        g.setColor(Color.red);
+
+        g.drawString("You lost", 550, 400);
+    }
+
+
+}
